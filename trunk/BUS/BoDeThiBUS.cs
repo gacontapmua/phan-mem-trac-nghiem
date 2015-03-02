59f23a1348da410a920a1a01c477498b7c9e4650
LeQuyenK41@gmail.com <LeQuyenK41@gmail.com@4120cef6-a4d0-7ef0-d686-86e65b060560>
2015-03-02 08:33:05 +0000

    Lập bộ đề thi tự động

---
+++ trunk/BUS/BoDeThiBUS.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace thitracnghiem.BUS
+{
+    public class BoDeThiBUS
+    {
+        QLTHITNDataContext db = new QLTHITNDataContext();
+
+        //Lưu bộ đề thi vào CSDL
+        public void SaveBoDeThi(  int socauhoi)
+        {
+            BODETHI bdt = new BODETHI();
+            bdt.NGAYTAO =DateTime.Today;
+            bdt.SOCAUHOI = socauhoi;
+            db.BODETHIs.InsertOnSubmit(bdt);
+
+            // Submit the change to the database. 
+            try
+            {
+                db.SubmitChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+                db.SubmitChanges();
+            }
+        }
+        public int GetMaBDTNew()
+        {
+            int q = 0;
+            q= (from a in db.BODETHIs
+                        orderby a.MABODT descending
+                        select a.MABODT).First();
+            return q;
+        }
+        //Lưu chi tiết bộ đề thi vào CSDL
+        public void SaveChiTietBoDeThi(  int socauhoi,ArrayList dsch)
+        {
+            //Lưu bộ đề thi
+            SaveBoDeThi( socauhoi);
+            //Lấy mã bộ đề thi mới nhất
+            int mabdt = GetMaBDTNew();
+
+            CHITIETBODETHI ctbdt;
+            foreach (CAUHOI item in dsch)
+            {
+                ctbdt = new CHITIETBODETHI();
+                ctbdt.MABODT = mabdt;
+                ctbdt.MACH = item.MACH;
+                db.CHITIETBODETHIs.InsertOnSubmit(ctbdt);
+
+                // Submit the change to the database. 
+                try
+                {
+                    db.SubmitChanges();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                    db.SubmitChanges();
+                }
+            }
+         
+          
+        }
+
+    }
+}
