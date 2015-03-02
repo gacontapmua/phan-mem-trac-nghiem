affedbd1a5f0cbf47e4aeada0e99c99e37a74dfb
LeQuyenK41@gmail.com <LeQuyenK41@gmail.com@4120cef6-a4d0-7ef0-d686-86e65b060560>
2015-03-02 08:33:05 +0000

    Lập bộ đề thi tự động

---
+++ BUS/CauHoiBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace thitracnghiem.BUS
+{
+    public class CauHoiBUS
+    {
+        QLTHITNDataContext db = new QLTHITNDataContext();
+        //Lấy danh sách câu hỏi theo nhóm
+        public ArrayList GetDSCauHoi (int manhom) 
+        {
+            ArrayList dsch = new ArrayList();
+            var query = from a in db.CAUHOIs
+                        where a.MANHOM == manhom
+                        select a;
+            foreach (CAUHOI item in query)
+            {
+                dsch.Add(item);
+            }
+            return dsch;
+        }
+    }
+}
---
+++ BUS/NhomVKBUS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thitracnghiem.BUS
+{
+    public class NhomVKBUS
+    {
+        QLTHITNDataContext db = new QLTHITNDataContext();
+
+       public  int GetMaNhomVK (string tennhom)
+        {
+        int query  = (from a in db.NHOMVKs
+                     where a.TENNHOM == tennhom
+                     select a.MANHOM).First() ;
+        return query;  
+        }
+    }
+}
